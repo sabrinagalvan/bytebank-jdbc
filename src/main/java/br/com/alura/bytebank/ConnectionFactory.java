032d96f846fdc6cac0ac41c702045ed821f2c9c2
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-11-04 00:24:48 -0300

    atualizando continuação do projeto de estudo

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,14 +10,22 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     public Connection recuperarConexao() {
-
         try {
-            return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=4151");
+            return createDataSource().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("4151");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 
 }
